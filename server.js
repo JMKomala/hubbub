e145989844e4cb6e814f7201c8bc2b43a51c5176
JM_Komala <jmkomala@gmail.com>
2017-12-07 20:16:47 -0800

    initial commit

--- server.js
+++ server.js
@@ -2,14 +2,12 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
-const mongo = require('./app/mongodb')
 const router = require('./app/routes')
 
 // initialize dotenv
-dotenv.config()
 
 // set our port
-const port = process.env.PORT || 8080
+const port =  8080
 
 // get all data/stuff of the body (POST) parameters
 // parse application/json
@@ -23,11 +21,7 @@ app.use(bodyParser.urlencoded({
 // register routes
 app.use(router)
 
-// start mongo connection pool, then start express app
-mongo.connect(process.env.MONGODB_URL)
-    .then(() => app.listen(port))
-    .then(() => console.log(`Magic happens on port: ${port}`))
-    .catch((err) => {
-        console.error(err)
-        process.exit(1)
-    })
+
+
+app.listen(port,console.log(`Magic happens on port: ${port}`))
+   
